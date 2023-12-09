3b10559070622c9c4f9394be4bd500511041014f
kimunave <bibliars.91824cogdhw@gmail.com>
2023-12-09 15:55:39 +0900

    categoryのコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/edit'
   get 'top/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts
