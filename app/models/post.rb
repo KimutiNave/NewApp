455ff23d9a39b3389d45131669b48b9807bd9cf7
kimunave <bibliars.91824cogdhw@gmail.com>
2024-01-28 22:13:06 +0900

    postにバリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, length: { maximum: 500 }
+  validates :other_file_name, length: { maximum: 500 }
+  validates :code_content, length: { maximum: 30000 }
+  validates :other_content, length: { maximum: 10000 }
 end
