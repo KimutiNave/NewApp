847b857c44843f80dfb3894babb411900637a73d
kimunave <bibliars.91824cogdhw@gmail.com>
2024-01-28 22:23:10 +0900

    another_postにバリデーションの追加

--- app/models/another_post.rb
+++ app/models/another_post.rb
@@ -1,3 +1,9 @@
 class AnotherPost < ApplicationRecord
+  validates :other_error_name, length: { maximum: 5000 }
+  validates :title, length: { maximum: 500 }
+  validates :other_file_name, length: { maximum: 500 }
+  validates :code_content, length: { maximum: 30000 }
+  validates :other_content, length: { maximum: 10000 }
+  
   enum status_error_name: ["Name Error", "No Method Error", "Template is missing", "LoadError", "Actioncontroller::UnknownFormat", "ArgumentError", "Syntax error", "その他"]
 end
