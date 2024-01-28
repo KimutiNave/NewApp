01097b8777ef3036a65891093ae58278896db3ee
kimunave <bibliars.91824cogdhw@gmail.com>
2024-01-28 21:50:00 +0900

    categoriesとpostsのcss編集とarticlesのコントローラーの編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @posts = Post.all
-    @another_posts = Another_post.all
+    @posts = Post.all(created_at: :desc)
+    @another_posts = Another_post.all(created_at: :desc)
   end
 end
