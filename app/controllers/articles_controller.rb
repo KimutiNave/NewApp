b4567a9b9fc14631485860446bbddf951bf68bbd
kimunave <bibliars.91824cogdhw@gmail.com>
2024-02-29 23:55:47 +0900

    設計変更によりarticlesを削除

--- app/controllers/articles_controller.rb
+++
@@ -1,9 +0,0 @@
-class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-  def index
-    @posts = current_user.posts.all.order(created_at: :desc).page(params[:page]) || []
-    @another_posts = current_user.another_posts.all.order(created_at: :desc).page(params[:page]) || []
-    @articles = @posts + @another_posts
-    @paginated_articles = Kaminari.paginate_array(@articles, total_count: @articles.count).page(params[:page])
-  end
-end
