23701fd861b04a07c189e530e71040a62f06c4e1
kimunave <bibliars.91824cogdhw@gmail.com>
2025-04-06 15:04:01 +0900

    DOMPurifyの追加とviewの編集

--- app/javascript/custom/text_limit.js
+++ app/javascript/custom/text_limit.js
@@ -1,13 +1,17 @@
 document.addEventListener('turbo:load', function() {
   const els = document.getElementsByClassName('card-text');
-  const len = 500;
+  const len = 1000;
 
   for(let i = 0; i < els.length; i++) {
     const el = els[i];
-    const str = el.textContent.trim();
+    const str = el.innerHTML.trim(); // innerHTMLを使用
 
     if (str.length > len) {
-        el.textContent = str.substring(0, len) + '…';
+      const truncatedStr = str.substring(0, len) + '…';
+      const sanitizedStr = DOMPurify.sanitize(truncatedStr); // サニタイズ
+      el.innerHTML = sanitizedStr; // innerHTMLに設定
+      const br = document.createElement('br'); // 改行用の要素を作成
+      el.appendChild(br); // 要素に追加
     }
   }
 });
